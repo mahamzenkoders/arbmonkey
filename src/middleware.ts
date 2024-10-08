5eeb21a9607c3b41c0eed4e48c37c7493ab0e285
Maham Sultana <mahamzenkoders@gmail.com>
2024-10-08 22:02:15 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get('accessToken')?.value;
 
-  const { pathname } = req.nextUrl.clone();
-  const publicRoutes = ['/login'];
-
-  const protectedRoutes = ['/', '/courses'];
-
-  if (token && publicRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
-
-  if (!token && protectedRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
 
   return NextResponse.next();
 }
